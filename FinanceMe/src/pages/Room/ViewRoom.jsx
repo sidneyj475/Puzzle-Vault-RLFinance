7ab48ec630c44aab240f54807b13c83dbe95d5e3
zachkr05 <zachkr05@gmail.com>
2025-02-08 14:54:07 -0500

    popping up the modal

--- FinanceMe/src/pages/Room/ViewRoom.jsx
+++ FinanceMe/src/pages/Room/ViewRoom.jsx
@@ -1,19 +1,56 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './ViewRoom.css';
+import QuestionModal from '../../modals/QuestionModal'; // adjust the import path
 
 function ViewRoom() {
   const [hover, setHover] = useState(false);
+  // State to show/hide the modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Handler to open the modal
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // Handler to close the modal
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <main className="view-room">
+      <svg
+        className={`lamp-border ${hover ? 'glow' : ''}`}
+        width="105"
+        height="83"
+        viewBox="0 0 105 83"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {/* Here is our onClick to open the modal */}
+        <path
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          onClick={handleOpenModal}
+          d="M1 66.5V81L104 82V66.5H99L98 52.5H95.5V26H86.5V14H80V2.5L20.5 1V14H12V26H7V66.5H1Z"
+          fill="transparent"
+          stroke="transparent"
+        />
+      </svg>
 
-<svg className={`lamp-border ${hover && 'glow'}`} width="105" height="83" viewBox="0 0 105 83" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} d="M1 66.5V81L104 82V66.5H99L98 52.5H95.5V26H86.5V14H80V2.5L20.5 1V14H12V26H7V66.5H1Z" fill="transparent" stroke="transparent"/>
-</svg>
-
+      {}
+      <QuestionModal
+        show={isModalOpen}
+        onCancel={handleCloseModal}
+        question="Which is the correct answer?"
+        options={['Option A', 'Option B', 'Option C', 'Option D']}
+        onOptionSelect={(selectedOption) => {
+          console.log('User selected:', selectedOption);
+          setIsModalOpen(false);
+        }}
+      />
     </main>
-  )
+  );
 }
 
-export default ViewRoom
+export default ViewRoom;
