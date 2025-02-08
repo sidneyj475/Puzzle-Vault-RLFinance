138a734f641bb3bbb2c0fe515cac319955d6f4b1
Carson Bates <carson.d.bates.27@dartmouth.edu>
2025-02-08 01:35:52 -0500

    -feat: routing

--- FinanceMe/src/routes/routes.jsx
+++ FinanceMe/src/routes/routes.jsx
@@ -1,16 +1,35 @@
 // Routes.jsx
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/loginPage/loginPage';
-import Signup from '../pages/signup';
+import { createBrowserRouter } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage/LoginPage';
+import SignUpPage from '../pages/SignUpPage/SignUpPage';
+import LandingPage from '../pages/LandingPage/LandingPage';
+import RoomSelectPage from '../pages/RoomSelectPage/RoomSelectPage';
+import Room from '../pages/Room/Room';
+import ProtectedRoute from './ProtectedRoute';
 
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/sign-up',
+    element: <SignUpPage />,
+  },
+  { path: '/landingPage', element: <LandingPage /> },
+  { path: '/roomSelect', element: <RoomSelectPage />},
+  { path: '/room/:roomGenre/:roomNumber', element: <Room />},
+  {
+    path: '/',
+    element: <ProtectedRoute />,
+       children:  [
+        { path: '/landingPage', element: <LandingPage /> },
+        { path: '/roomSelect', element: <RoomSelectPage />},
+        { path: '/room/:roomGenre/:roomNumber', element: <Room />}
+      ]
+  },
+]);
 
-export default AppRoutes;
+export default router
+
