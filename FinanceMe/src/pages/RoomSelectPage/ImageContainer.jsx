0551e8addb118b9bfe18bbb5006a5b446f82f858
Carson Bates <carson.d.bates.27@dartmouth.edu>
2025-02-08 15:29:09 -0500

    -feat: fix room select page

--- FinanceMe/src/pages/RoomSelectPage/ImageContainer.jsx
+++ FinanceMe/src/pages/RoomSelectPage/ImageContainer.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './ImageContainer.css';
+import Star from '../../assets/CuteStarSprite.png';
 
-function ImageContainer({ src, alt, onClick}) {
+function ImageContainer({ src, alt, onClick, difficulty}) {
   return (
       <div className="room">
+        <p>hello</p>
+        <div className="room__description">
+          <div className="room__description__content">
+          <p>Difficulty: </p>
+          {difficulty.map((diff, index) => {
+            return <img style={{height: "70px", width: "70px"}}key={index} src={Star}/>
+          })}
+          </div>
+        </div>
         <img src={src} alt={alt} onClick={onClick} />
       </div>
   );
